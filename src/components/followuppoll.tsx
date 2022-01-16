499f4a7ddfad79f5fdac9934e551af8e929520f7
or <ororfeldman4@gmail.com>
2022-01-16 13:09:10 +0200

    update pollsview

--- src/components/followuppoll.tsx
+++ src/components/followuppoll.tsx
@@ -6,19 +6,19 @@ import Box from "@mui/material/Box";
 import Poll from "./poll";
 import { PollTree } from "../PollTree";
 import axios from "axios";
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import { Modal } from "@mui/material";
 import { Button } from "@mui/material";
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
@@ -30,58 +30,75 @@ export default function FollowupPoll({}: FollowupPollProps) {
     father_poll_id: number | null;
     poll_id: number;
     root_poll_id: number;
-    answers: { label: string,id:number }[];
+    answers: { label: string; id: number }[];
     label: string;
-    id: number
+    question: string;
+    id: number;
   }[] = [];
   const [followupBranch, setfollowupBranch] = useState(null);
   const [answer, setanswer] = useState(null);
-  const [pollsState, setPolls] = useState({called:false,polls:polls});
-  const [modalState,setModalState] = useState({isOpen: false,title:"",message:""})
+  const [pollsState, setPolls] = useState({ called: false, polls: polls });
+  const [modalState, setModalState] = useState({
+    isOpen: false,
+    title: "",
+    message: "",
+  });
   const emptyList: never[] = [];
   const pt = new PollTree("");
-  if (pollsState.called ==false){
+  if (pollsState.called == false) {
     axios.get("/api/polls").then((response) => {
       if (response.data.success) {
         let new_polls = response.data.polls;
-        new_polls.forEach((p: any,i:number) => {
+        new_polls.forEach((p: any, i: number) => {
           p.label = p.question;
-          p.id = i
-          p.answers=p.answers.map((a:any,j:any)=>{return {label:a,id:j}})
+          p.id = i;
+          p.answers = p.answers.map((a: any, j: any) => {
+            return { label: a, id: j };
+          });
         });
-        setPolls({called:true,polls:new_polls});
+        setPolls({ called: true, polls: new_polls });
       }
     });
   }
-  
-  const sendPoll = ()=>{
-    const ans:any = answer
-    if (pt.question.length!= 0 && followupBranch!=null && answer!=null){
-        console.log(pt)
-        let poll = pollsState.polls[followupBranch]
-        axios.post(
-            "/api/followuppolls",
-            {poll: {
-              "root_poll_id": poll.root_poll_id,
-              "father_poll_id":  poll.poll_id,
-              "answer_number": ans.id,
-              "question": pt.question,
-              "answers":pt.answers.filter((a)=>a.length!=0)
-            }}
-          ).then(response=>{
-            if(response.data.success){
-                setModalState({isOpen: true,title:"Success!",message:"Poll sent successfully"})
-                //const  pt = new PollTree("")
-            }
-            else
-                setModalState({isOpen: true,title:"Failure!",message: response.data.error_description})
+
+  const sendPoll = () => {
+    const ans: any = answer;
+    if (pt.question.length != 0 && followupBranch != null && answer != null) {
+      console.log(pt);
+      let poll = pollsState.polls[followupBranch];
+      axios
+        .post("/api/followuppolls", {
+          poll: {
+            root_poll_id: poll.root_poll_id,
+            father_poll_id: poll.poll_id,
+            answer_number: ans.id,
+            question: pt.question,
+            answers: pt.answers.filter((a) => a.length != 0),
+          },
         })
+        .then((response) => {
+          if (response.data.success) {
+            setModalState({
+              isOpen: true,
+              title: "Success!",
+              message: "Poll sent successfully",
+            });
+            //const  pt = new PollTree("")
+          } else
+            setModalState({
+              isOpen: true,
+              title: "Failure!",
+              message: response.data.error_description,
+            });
+        });
+    } else {
+      setModalState({
+        isOpen: true,
+        title: "Failure!",
+        message: "Question for the poll is not set",
+      });
     }
-    else{
-        setModalState({isOpen: true,title:"Failure!",message: "Question for the poll is not set"})
-    }
-    
-  }
+  };
 
   return (
     <Box
@@ -113,16 +130,18 @@ export default function FollowupPoll({}: FollowupPollProps) {
         style={{ marginLeft: "auto", marginRight: "auto" }}
         disablePortal
         options={
-          followupBranch != null ? pollsState.polls[followupBranch].answers : emptyList
+          followupBranch != null
+            ? pollsState.polls[followupBranch].answers
+            : emptyList
         }
         sx={{ width: 300 }}
         renderInput={(params) => (
           <TextField {...params} label="Followup answer" />
         )}
         value={answer != null ? answer : null}
-        onChange={(e: any, value: any) =>{
-          setanswer(value != null ? value : null)}
-        }
+        onChange={(e: any, value: any) => {
+          setanswer(value != null ? value : null);
+        }}
         isOptionEqualToValue={(option, value) => option.label == value.label}
       />
       {followupBranch == null || answer == null ? null : (
@@ -134,11 +153,13 @@ export default function FollowupPoll({}: FollowupPollProps) {
         ></Poll>
       )}
       <Stack spacing={2} direction="row">
-      <Button onClick={sendPoll} style={{margin: "auto"}}>Submit</Button>
+        <Button onClick={sendPoll} style={{ margin: "auto" }}>
+          Submit
+        </Button>
       </Stack>
       <Modal
         open={modalState.isOpen}
-        onClose={()=>setModalState({isOpen: false,title:"",message:""})}
+        onClose={() => setModalState({ isOpen: false, title: "", message: "" })}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
@@ -147,7 +168,7 @@ export default function FollowupPoll({}: FollowupPollProps) {
             {modalState.title}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {modalState.message}
+            {modalState.message}
           </Typography>
         </Box>
       </Modal>
--- src/components/pollsview.tsx
+++ src/components/pollsview.tsx
@@ -1,26 +1,34 @@
 import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import PollView from "../pollview";
-
-const questions: { desc: string; answers: string[] }[] = [
-  { desc: "how are you?", answers: ["good", "bad"] },
-  { desc: "are you sure?", answers: ["yes", "no"] },
-];
+import axios from "axios";
 
 export interface PollsViewProps {}
 
 export default function PollsView() {
+  const polls: { question: string; answers: string[] }[] = [];
+  const [pollsState,setPollsState] = useState({ called: false, polls: polls });
+
+  if (pollsState.called == false) {
+    axios.get("/api/polls").then((response) => {
+      if (response.data.success) {
+        const new_polls = response.data.polls;
+        setPollsState({ called: true, polls: new_polls });
+      }
+    })}
+
   return (
     <Box
       component="div"
       style={{ width: "100%", margin: 8, textAlign: "center" }}
     >
       <div style={{ marginLeft: "auto", marginRight: "auto" }}>
-        {questions.map((question, index) => (
+        {pollsState.polls.map((poll, index) => (
           <PollView
             key={index}
-            question={question.desc}
-            answers={question.answers}
+            question={poll.question}
+            answers={poll.answers}
           ></PollView>
         ))}
       </div>
