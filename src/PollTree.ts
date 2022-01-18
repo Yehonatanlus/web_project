ef91d64e94c5aa7c91f62d6927adf1425b1f30d5
Yehonatanlus <yhonatanlus@gmail.com>
2022-01-18 23:41:02 +0200

    final

--- src/PollTree.ts
+++ src/PollTree.ts
@@ -12,7 +12,7 @@ export class PollTree {
   ToJsonInner(poll_tree: PollTree | null) {
     if (poll_tree == null || poll_tree.question.length == 0) return null;
     let json: any = { question: poll_tree.question, answers: [] };
-    for (let i = 0; i < this.answers.length; i++) {
+    for (let i = 0; i < poll_tree.answers.length; i++) {
       if (poll_tree.answers[i].length == 0) continue;
       let inner_json = this.ToJsonInner(poll_tree.followupPolls[i]);
       json["answers"].push({ desc: poll_tree.answers[i], poll: inner_json });
